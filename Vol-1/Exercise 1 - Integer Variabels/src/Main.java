22d4eb21502f63c593e4ecbf0b39760ea5b58405
Michael <mpeder75@gmail.com>
2023-06-26 21:33:03 +0200

    Minor updates to module names

---
+++ Vol-1/Exercise 1 - Integer Variabels/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+    /*
+    Write a program that declares three variables named age1, age2, and age3.
+    Assign any age you wish to each of the three variables, then use the println() method
+    to output the ages to the screen separated by commas.
+     */
+
+        // variabler declares og assignes værdier
+        int age1 = 20;
+        int age2 = 30;
+        int age3 = 40;
+
+        // variabler printes
+        System.out.println("Ag1 is " + age1 + "\nAge2 is " +age2 + "\nAge3 is " + age3);
+
+    }
+}
+
+
---
+++ Vol-1/Exercise 2 -  Integer Assign Variabels/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Lisa has 34 dollars, John has 205 dollars, and Jenny has negative 20 dollars .
+        Write a program that assigns these dollar amounts to variables, then
+        output the following to the screen, exactly as written.
+
+        Lisa has 34 $, and John has 205 $.
+        Jenny has negative money, and she has -20 $!
+         */
+
+        int lisaMoney = 34;
+        int johnMoney = 205;
+        int jennyMoney = -20;
+
+        System.out.println("Lisa has " + lisaMoney + "$, and John has " + johnMoney
+                            + "$.\nJenny has negative money, and she has " + jennyMoney + "$!"  );
+
+    }
+}
+
