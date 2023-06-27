bc7291778b318303dc338958e7236cae754eb667
Michael <mpeder75@gmail.com>
2023-06-27 09:04:50 +0200

    Create Vol-1 c.Arithemetic with int variabels

---
+++ Vol-1/a. System.out.println/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Insert new line after \n
+        System.out.println("This will create a new line,\nthis is the NEW line");
+
+        System.out.println("Countdown:\n5\n4\n3\n2\n1");
+
+
+        // Insert TAB after \t
+        System.out.println("Do you like me?\t\tYes\t\tNo");
+
+
+        // Example
+        System.out.println("Home Town Burger Restaurant");
+        System.out.println("Hours:\nMon - Fri, 9am - 9pm");
+        System.out.println("\nLowest prices in town");
+        System.out.println("Lunch\tDinner");
+        System.out.println("$5.95\t$11.95");
+
+    }
+}
---
+++ Vol-1/b. Declaring integer variabels/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Declare integer variabel
+        int variabel1;
+
+        /// Declare FLERE variabler af samme datatype
+        int variabel2, variabel3;
+
+        // Assign variabler værdier
+        variabel1 = 9;
+        variabel2 = 24;
+        variabel3 = 16;
+
+        // Declare og Assign variabel på een gang
+        int myVariabel = 50;
+
+        // Print variabel
+        System.out.println("The value og variabel1 is " + variabel1);
+        System.out.println("Variabel2 is " + variabel2 + "\nVariabel3 is " + variabel3);
+        System.out.println(myVariabel);
+
+
+    }
+}
