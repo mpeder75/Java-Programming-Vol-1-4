8fbfba734da1deb3169badf1d4e07c5607bc9340
Michael <mpeder75@gmail.com>
2023-07-06 11:23:49 +0200

    Create Vol-1, Floating Points + Updated Datatypes

---
+++ Vol-1/Exercise 17 - Char/src/ExerciseMedBogstaver.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Exercise 1
+        Write a program that uses a for loop to print out
+        the alphabet from A->Z as follows:
+
+        A   a
+        B   b
+        C   c
+        D   d
+        ...and on  until Z.
+
+        Note that there is a tab between the columns.
+        Uppercase letter is on the left and lowercase on the right.
+         */
+
+        // Her opsættes variabler, der skal have char værdier
+        char upperCase = 'A';
+        char lowerCase = 'a';
+
+
+        for (int i = 1; i <= 26; i++)
+        {
+            // først printes nuværende værdi af variabler
+            System.out.println(upperCase + "\t" + lowerCase);
+
+            // derefter incrementes variabler, så NÆSTE iteration printer opdaterede variabler
+            upperCase++;
+            lowerCase++;
+        }
+
+    }
+}
